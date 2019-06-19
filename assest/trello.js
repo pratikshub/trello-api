9547e222ade32f6f79d7319bd05ee888a4fc7ec5
Shubham Pratik <shubham.pratik@mountblue.io>
2019-06-19 22:28:16 +0530

    adds the fetch data and display on webpage using dom

--- assest/trello.js
+++ assest/trello.js
@@ -2,12 +2,50 @@ let key = 'b15cd266cff4107fb914ae08ff3fa96b';
 let token = 'd9c2b0cd9abcd9150dbcac22dfad5c8ab15e06667ec6298c274065d46ff4e40e';
 let listid = '5d09bd3783ab92349ebbbe77';
 
-function fetchUrl(url){
-    return fetch(url).then(element => element.json() )
+function fetchUrl(url) {
+    return fetch(url).then(element => element.json())
 }
-async function fetchListCard(listid,key,token){
-    let data=await fetchUrl(`https://api.trello.com/1/lists/${listid}/cards?${key}&token=${token}`);
-    console.log(data)
-   
+async function fetchCheckListId(listid, key, token) {
+    let data = await fetchUrl(`https://api.trello.com/1/lists/${listid}/cards?${key}&token=${token}`);
+    return data.reduce((acc, element) => {
+        // console.log(element.id)
+        acc.push(element['id']);
+        return acc;
+    }, []);
 }
-fetchListCard(listid,key,token);
+async function fetchChecklistItem() {
+    let idChecklists = await fetchCheckListId(listid, key, token);
+    console.log(idChecklists)
+    idChecklists.forEach(async (element) => {
+        url = `https://api.trello.com/1/cards/${element}/checklists?${key}'&token=${token}`;
+        let checklist = await fetchUrl(url);
+
+        checklist['0']['checkItems'].forEach(async (item) => {
+            let html = `<li class="list-group-item">
+            <input type="checkbox" class="check" ${item.state === "complete" ? "checked" : null} data-state="${item.state}" card-id=${element} checkitem-Id=${item.id}>
+            <span class=${item.state} id="itemName">${item.name}</span>
+            <button class="btn btn-danger" id='delete'>X</button>
+            </li>
+            `
+            $('.list-group').append(html);
+
+
+        });
+    })
+    // $(document).on('change', 'input:checkbox', updateData);
+    // $(document).on('click', 'a', deleteData);
+}
+
+fetchChecklistItem();
+// $('.list-group-item').on('click',function () {
+//     console.log("kfm")
+//     })
+
+
+// // document.getElementById("").addEventListener("click", myFunction);
+
+// function myFunction() {
+//   alert ("Hello World!");
+// }
+{/* <input type="checkbox" class="check" null="" data-state="incomplete" card-id="5d09bd6f9a14106641b4c37d" checkitem-id="5d09bd6f9a14106641b4c380"></input> */ }
+
